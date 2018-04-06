dad05dae4702b8a619131b601e4f8f6737639c8e
Evans Musomi <evans.musomi@gmail.com>
2018-04-06 02:17:48 +0000

    adds play actions and create play view files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,41 @@
 class PlaysController < ApplicationController
+    before_action :find_play, only:[:edit, :update, :show, :destroy]
+    
     def index
+    end
+    
+    def new
+        @play = Play.new
+    end
+    
+    def create
+        @play = Play.new(play_params)
+        if @play.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def show
+    end
+    
+    def destroy
+    end
+    
+    private
+    
+    def find_play
+        @play = Play.find(params[:id])
+    end
+    
+    def play_params
+        params.require(:play).permit(:title, :description, :director)
     end
 end
